7b199071a37cc1a024e4fb5331089279caa45168
roswellt <qtrido@gmail.com>
2017-11-19 00:42:32 -0500

    check button functionality added

--- sudoku.js
+++ sudoku.js
@@ -26,9 +26,9 @@ $(document).ready(function() {
 });
 
 // Check column for duplicates
-function checkColumn(row, column, number) {
+function checkColumn(column, number) {
   var count = 0;
-  var duplicate = false;
+  var valid = true;
 
   for (var rowNum = 0; rowNum < 9; rowNum++) {
     if (sudokuMatrix[rowNum][column] == number) {
@@ -36,16 +36,14 @@ function checkColumn(row, column, number) {
     }
   }
   if (count > 1) {
-    duplicate = true;
+    valid = false;
   }
-  if (duplicate) {
-    //TODO
-  }
+  return valid;
 }
 // Check row for duplicates
-function checkRow(row, column, number) {
+function checkRow(row, number) {
   var count = 0;
-  var duplicate = false;
+  var valid = true;
 
   for (var columnNum = 0; columnNum < 9; columnNum++) {
     if (sudokuMatrix[row][columnNum] == number) {
@@ -53,18 +51,16 @@ function checkRow(row, column, number) {
     }
   }
   if (count > 1) {
-    duplicate = true;
+    valid = false;
   }
-  if (duplicate) {
-    //TODO
-  }
+  return valid;
 }
 // Check square for duplicates
 function checkSquare(row, column, number) {
   var startOfRow = 6;
   var startOfColumn = 6;
   var count = 0;
-  var duplicate
+  var valid = true;
 
   for (startOfRow; startOfRow >= 0; startOfRow -= 3) {
     if (row >= startOfRow) {
@@ -83,12 +79,36 @@ function checkSquare(row, column, number) {
       }
     }
     if (count > 1) {
-      duplicate = true;
+      valid = false;
     }
   }
-  if (duplicate) {
-    //TODO
+  return valid;
+}
+
+function checkBoard() {
+  var rows = document.getElementsByClassName('sudoku-row');
+  var valid = true;
+
+  for (var i = 0; i < rows.length; i++) {
+    if (!valid) {
+      break;
+    }
+    var columns = $(rows[i]).children("div");
+    for (var j = 0; j < columns.length; j++) {
+      var number;
+      if ($(columns[j]).children("h1").length > 0) {
+        number = $(columns[j]).text();
+      } else {
+        number = $(columns[j]).children("input").get(0).value;
+      }
+      console.log(number);
+      if (!(number >= 1 || number <= 9) || number == "" || !checkRow(i, number) || !checkColumn(j, number) || !checkSquare(i, j, number)) {
+        valid = false;
+        break;
+      }
+    }
   }
+  return valid;
 }
 
 // Limit the input to 1 digit from 1-9
@@ -100,9 +120,20 @@ $('.number').keypress(function(event) {
     var position = $(this).parent().data('position');
     // console.log(position);
     sudokuMatrix[position.row][position.column] = String.fromCharCode(event.keyCode);
-    checkColumn(position.row, position.column, String.fromCharCode(event.keyCode));
-    checkRow(position.row, position.column, String.fromCharCode(event.keyCode));
+    checkColumn(position.column, String.fromCharCode(event.keyCode));
+    checkRow(position.row, String.fromCharCode(event.keyCode));
     checkSquare(position.row, position.column, String.fromCharCode(event.keyCode));
     // console.log(sudokuMatrix);
   }
 });
+
+$("#checkButton").click(function() {
+  var modalText;
+  if(checkBoard) {
+    modalText = "Not a valid solution!";
+  } else {
+    modalText = "Solved!";
+  }
+  $("#ex1").text(modalText);
+  $("#ex1").modal();
+})
